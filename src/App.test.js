e4ecd749474d5a54fd16d9cff354d647f23c2c61
Joram Obbus <joramobbus@gmail.com>
2022-07-14 21:02:05 -0700

    Finished

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import renderer from 'react-test-renderer'
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders a snapshot', () => {
+  const tree = renderer.create(<App/>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
